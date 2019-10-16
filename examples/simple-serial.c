e75eebb90b4555cf20494e982db9da08c785a036
ezra buchla <emb@catfact.net>
2019-10-15 19:10:40 -0700

    update front matter

--- examples/simple-serial.c
+++ examples/simple-serial.c
@@ -12,10 +12,14 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ 
+ * modified 2019 by ez b
+ 
  */
 
 /**
- * simple.c
+ * simple-serial.c
+ * run it with a tty device path!
  * press a button to toggle it!
  */
 
